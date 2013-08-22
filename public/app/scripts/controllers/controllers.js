a16748bb7ea22d2428d7de91eafc7cfd12ac850c
Matteo Gildone <matteo.gildone@gmail.com>
2013-08-22 15:52:51 +0100

    Structure refactor

--- public/app/scripts/controllers/controllers.js
+++ public/app/scripts/controllers/controllers.js
@@ -8,14 +8,14 @@ app.controller('HomeCtrl', ['$scope', 'Page', function($scope, Page) {
 
 app.controller('TopNewsCtrl', ['$scope', 'newsData', function($scope, newsData) {
     $scope.loading = true;
-    $scope.topNews = newsData.getTopNews();
+    $scope.topNews = newsData.getNewsList();
     $scope.topNews.then(function () {
         $scope.loading = false;
     });
 }]);
 
 app.controller('NewsCtrl', ['$scope', 'newsData', function($scope, newsData) {
-    var newsObj = newsData.getNews();
+    var newsObj = newsData.getNewsList({number: 10});
     $scope.currentPage = 1;
 
     $scope.loading = true;
@@ -30,7 +30,7 @@ app.controller('NewsCtrl', ['$scope', 'newsData', function($scope, newsData) {
 
     $scope.moreContent = function () {
         $scope.loading = true;
-        var more = newsData.getPageNews($scope.currentPage);
+        var more = newsData.getNewsList({number: 10, page: $scope.currentPage});
 
         more.then(function (newsList) {
             angular.forEach(newsList.posts, function (value, key) {
@@ -44,7 +44,7 @@ app.controller('NewsCtrl', ['$scope', 'newsData', function($scope, newsData) {
 
 app.controller('DetailCtrl', ['$scope', '$routeParams', 'newsData', 'Page', function($scope, $routeParams, newsData, Page) {
     var slug = (($routeParams.slug) ? $routeParams.slug : '');
-    $scope.oneNews = newsData.getOneNews(slug);
+    $scope.oneNews = newsData.getNewsDetail(slug);
     $scope.oneNews.then(function (oneNews) {
         Page.setTitle(oneNews.title);
     });
--- public/app/scripts/services/services.js
+++ public/app/scripts/services/services.js
@@ -1,40 +1,26 @@
 app.factory('newsData', ['$http', '$log', '$q', function ($http, $log, $q) {
     return {
-        getTopNews: function () {
-            var deferred = $q.defer();
+        getNewsList: function (params) {
+            var deferred = $q.defer(),
+                attrs = params || {},
+                number = attrs.number || 5,
+                page = attrs.page || 1;
 
             $http({
                 method:'JSONP',
-                url: "https://public-api.wordpress.com/rest/v1/sites/metrouk2.wordpress.com/posts/?number=5&callback=JSON_CALLBACK"
+                url: 'https://public-api.wordpress.com/rest/v1/sites/metrouk2.wordpress.com/posts/?number=' + number + '&page=' + page + '&callback=JSON_CALLBACK'
             })
-            .success(function (data, status, headers, config) {
-                deferred.resolve( data );
-            })
-            .error(function (data, status, headers, config) {
-                deferred.reject( status );
-            });
+                .success(function (data, status, headers, config) {
+                    deferred.resolve( data );
+                })
+                .error(function (data, status, headers, config) {
+                    deferred.reject( status );
+                });
 
             return deferred.promise;
 
         },
-        getNews: function () {
-            var deferred = $q.defer();
-
-            $http({
-                method:'JSONP',
-                url: "https://public-api.wordpress.com/rest/v1/sites/metrouk2.wordpress.com/posts/?number=10&page=1&callback=JSON_CALLBACK"
-            })
-            .success(function (data, status, headers, config) {
-                deferred.resolve( data );
-            })
-            .error(function (data, status, headers, config) {
-                deferred.reject( status );
-            });
-
-            return deferred.promise;
-
-        },
-        getOneNews: function (slug) {
+        getNewsDetail: function (slug) {
             var deferred = $q.defer();
 
             $http({
@@ -50,25 +36,7 @@ app.factory('newsData', ['$http', '$log', '$q', function ($http, $log, $q) {
 
             return deferred.promise;
 
-        },
-        getPageNews: function (page) {
-            var deferred = $q.defer();
-
-            $http({
-                method:'JSONP',
-                url: "https://public-api.wordpress.com/rest/v1/sites/metrouk2.wordpress.com/posts/?number=10&page=" + (page +=1) + "&callback=JSON_CALLBACK"
-            })
-                .success(function (data, status, headers, config) {
-                    deferred.resolve( data );
-                })
-                .error(function (data, status, headers, config) {
-                    deferred.reject( status );
-                });
-
-            return deferred.promise;
-
         }
-
     };
 }]);
 
